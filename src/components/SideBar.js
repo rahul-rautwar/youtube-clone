9ebac013649e29266a2e703de2ad31649aafc985
rahul-rautwar <rahulrautwar@gmail.com>
2023-03-31 00:04:04 +0530

    create sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="p-5 shadow-lg w-48">
+      <ul className="">
+        <li className="">Home</li>
+        <li className="">Sports</li>
+        <li className="">Videos</li>
+        <li className="">Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul className="">
+        <li className="">Music</li>
+        <li className="">Sports</li>
+        <li className="">Gaming</li>
+        <li className="">Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul className="">
+        <li className="">Music</li>
+        <li className="">Sports</li>
+        <li className="">Gaming</li>
+        <li className="">Movies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
