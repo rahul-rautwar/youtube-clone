a162ea3c2013462b1f781feb6fdfff9e52681cd2
rahul-rautwar <rahulrautwar@gmail.com>
2023-04-05 14:44:06 +0530

    feat(app): add cz-customizable for commit message

---
+++ .cz-config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  // https://www.npmjs.com/package/cz-customizable
+  types: [
+    { value: "feat", name: "feat:     A new feature" },
+    { value: "fix", name: "fix:      A bug fix" },
+    { value: "docs", name: "docs:     Documentation only changes" },
+    {
+      value: "style",
+      name: "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
+    },
+    {
+      value: "refactor",
+      name: "refactor: A code change that neither fixes a bug nor adds a feature",
+    },
+    {
+      value: "perf",
+      name: "perf:     A code change that improves performance",
+    },
+    { value: "test", name: "test:     Adding missing tests" },
+    {
+      value: "chore",
+      name: "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
+    },
+    { value: "revert", name: "revert:   Revert to a commit" },
+    { value: "WIP", name: "WIP:      Work in progress" },
+  ],
+
+  scopes: [
+    { name: "search" },
+    { name: "livechat" },
+    { name: "comments" },
+    { name: "watchpage" },
+    { name: "home" },
+    { name: "app" },
+  ],
+  allowCustomScopes: false,
+  usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
+  allowTicketNumber: false, // make true when ticket number required
+  isTicketNumberRequired: false, // make true when ticket number required
+  // ticketNumberPrefix: "TICKET-", // when ticket number required
+  // ticketNumberRegExp: "\\d{1,5}", // when ticket number required
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+    scopeOverrides: {
+      fix: [
+        {name: 'merge'},
+        {name: 'style'},
+        {name: 'e2eTest'},
+        {name: 'unitTest'}
+      ]
+    },
+    */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    // ticketNumber: 'Please enter the story or defect number',
+    scope: "\nWhat is the SCOPE of this change (optional):",
+    // used if allowCustomScopes is true
+    // customScope: 'Denote the SCOPE of this change:',
+
+    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: "List any BREAKING CHANGES (optional):\n",
+    // footer: "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
+    confirmCommit: "Are you sure you want to proceed with the commit above?",
+  },
+
+  allowBreakingChanges: ["feat", "fix"],
+  // skip any questions you want
+  skipQuestions: ["footer"],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
